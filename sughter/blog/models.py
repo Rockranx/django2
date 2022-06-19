49b4838c7ac858d0f6bfe59fe6037b906f81b979
Rockranx <sughtera@gmail.com>
2022-06-19 13:49:17 +0100

    first

---
+++ sughter/blog/models.py
@@ -0,0 +1,13 @@
+from turtle import title
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text= models.TextField()
+    author = models.ForeignKey(User)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
+    
+
+# Create your models here.
